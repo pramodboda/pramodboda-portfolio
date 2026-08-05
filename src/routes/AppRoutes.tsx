b2bfb54db13393cb2001d3aea625ad482d2a2a4b
pramodboda <pramodkumarboda@gmail.com>
2026-08-05 17:01:54 +0530

    Updates: UI

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,3 +1,16 @@
 import {Router , Route} from "react-router-dom";
 
-import Home from "./AllProjects"
+import Home from "./pages/Home/Home";
+import AllProjects from ".pages/AllProjects/AllProjects";
+
+
+function AppRoutes(){
+    return(
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/projects" element={<Projects/>}/>
+
+            
+        </Routes>
+    )
+}
